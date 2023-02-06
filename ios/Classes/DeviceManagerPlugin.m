a61a147892538158f9ea1ae29ff963c11a7969a4
Ivan Herrera Olivares <ivan.herreraolivares@uantwerpen.be>
2023-02-06 16:10:50 +0100

    Fix import

--- ios/Classes/DeviceManagerPlugin.m
+++ ios/Classes/DeviceManagerPlugin.m
@@ -1,11 +1,11 @@
 #import "DeviceManagerPlugin.h"
-#if __has_include(<flutter_mcu_manager/device_manager-Swift.h>)
-#import <flutter_mcu_manager/device_manager-Swift.h>
+#if __has_include(<flutter_mcu_manager/flutter_mcu_manager-Swift.h>)
+#import <flutter_mcu_manager/flutter_mcu_manager-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "device_manager-Swift.h"
+#import "flutter_mcu_manager-Swift.h"
 #endif
 
 @implementation DeviceManagerPlugin
